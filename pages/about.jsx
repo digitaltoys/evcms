f3d421b331e587a2acf4a78029260afab62da227
digitaltoys <pepsi2@gmail.com>
2023-07-05 15:04:44 +0900

    app폴더 위치 변경

---
+++ pages/about.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import RootLayout from '../app/layout'
+
+export default function About() {
+  return (
+    // <RootLayout>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>About</div>
+    </main>
+    // </RootLayout>
+  )
+}
