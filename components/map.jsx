5fcdfe2621311b82c7af0c760d50455950a12d43
dldnjswns31 <dldnjswns32@gmail.com>
2023-07-14 14:05:54 +0900

    useEffect 최소화(렌더링 개선), 화면 내 마커만 fetching

--- components/map.jsx
+++ components/map.jsx
@@ -2,7 +2,11 @@ import { useEffect, useRef, useState } from "react";
 import { useAtom } from "jotai";
 
 import "../styles/components/map.css";
-import { getStationDetail, getStationList } from "../apis/evApi";
+import {
+  getBoundStationList,
+  getStationDetail,
+  getStationList,
+} from "../apis/evApi";
 import {
   mapAtom,
   selectedMarkerAtom,
@@ -14,8 +18,8 @@ const Map = () => {
   const [stationList, setStationList] = useState(null);
   const [markerList, setMarkerList] = useState(null);
   const stationOverlayRef = useRef(null);
+  const mapRef = useRef(null);
 
-  const [map, setMap] = useAtom(mapAtom);
   const [selectedMarker, setSelectedMarker] = useAtom(selectedMarkerAtom);
   const [selectedMarkerDetail, setSelectedMarkerDetail] = useAtom(
     selectedMarkerDetailAtom
@@ -23,26 +27,9 @@ const Map = () => {
 
   const KAKAOMAP_API_KEY = "213d725ddb120155aa57f8ae612ed6d4";
 
-  // 현재 위치 받아오기
+  // 카카오맵 Init
   useEffect(() => {
-    if (navigator.geolocation) {
-      console.log(navigator.geolocation);
-      navigator.geolocation.getCurrentPosition(
-        (pos) => {
-          const { latitude, longitude } = pos.coords;
-          setGps({ lat: latitude, lng: longitude });
-        },
-        () => {},
-        { enableHighAccuracy: true }
-      );
-    } else {
-      setGps({ lat: 37.5696304, lng: 126.9821706 });
-    }
-  }, []);
-
-  // 카카오맵 Load
-  useEffect(() => {
-    console.log(gps);
+    console.log("%c Init useEffect", "color:blue");
     const script = document.createElement("script");
 
     script.async = true;
@@ -50,55 +37,89 @@ const Map = () => {
 
     document.head.appendChild(script);
 
+    // 카카오맵 load 시 실행
     const onLoadKakaoMap = () => {
-      window.kakao.maps.load(() => {
+      window.kakao.maps.load(async () => {
+        // 현재 위치 받아오기
+        const getGps = () => {
+          return new Promise((res, rej) => {
+            navigator.geolocation.getCurrentPosition(
+              (pos) => {
+                const { latitude, longitude } = pos.coords;
+                res({ lat: latitude, lng: longitude });
+              },
+              () => {
+                console.log("failed to get GPS");
+                res({ lat: 37.5696304, lng: 126.9821706 });
+              },
+              { enableHighAccuracy: true }
+            );
+          });
+        };
+
+        const { lat, lng } = await getGps();
+        setGps({ lat, lng });
+
+        // 카카오지도 생성
         const container = document.querySelector("#map");
         const options = {
-          // center: new window.kakao.maps.LatLng(latitude, longitude),
-          center: new window.kakao.maps.LatLng(gps.lat, gps.lng),
+          center: new window.kakao.maps.LatLng(lat, lng),
           level: 2,
         };
         const newMap = new window.kakao.maps.Map(container, options);
-        setMap(newMap);
+        mapRef.current = newMap;
+
+        // 충전소 data fetching
+        const fetchEvStationList = async () => {
+          try {
+            const {
+              ha: lngSW,
+              qa: latSW,
+              oa: lngNE,
+              pa: latNE,
+            } = mapRef.current.getBounds();
+            console.log("ha", lngSW, "qa", latSW, "oa", lngNE, "pa", latNE);
+            console.log(mapRef.current.getBounds());
+            const params = {
+              s: latSW,
+              w: lngSW,
+              n: latNE,
+              e: lngNE,
+            };
+
+            const data = await getBoundStationList(params);
+            console.log(data);
+            setStationList(data);
+          } catch (err) {
+            throw err;
+          }
+        };
+        fetchEvStationList();
       });
     };
 
     script.addEventListener("load", onLoadKakaoMap);
 
-    // 충전소 data fetching
-    const fetchEvStationList = async () => {
-      try {
-        const data = await getStationList();
-        setStationList(data);
-      } catch (err) {
-        throw err;
-      }
-    };
-    fetchEvStationList();
-
-    // 현재 위치 받아오기
-
     return () => script.removeEventListener("load", onLoadKakaoMap);
-  }, [gps]);
+  }, []);
 
   // 충전소 data에 따라 마커 생성
   useEffect(() => {
-    if (stationList && map) {
+    console.log("%c stationList useEffect", "color: blue");
+    if (stationList) {
       // 마커 생성 및 추가, 이벤트 바인딩
       for (let mk of stationList) {
         const { lat, lng, statId, statNm } = mk;
         const markerPosition = new window.kakao.maps.LatLng(lat, lng);
         const marker = new window.kakao.maps.Marker({
           position: markerPosition,
-          map,
+          map: mapRef.current,
           title: statNm,
         });
         marker.id = statId;
 
         // 마커 클릭 이벤트 추가
         window.kakao.maps.event.addListener(marker, "click", () => {
-          console.log("click");
-          console.log(stationOverlayRef.current);
           if (
             stationOverlayRef.current &&
             stationOverlayRef.current.id === marker.id
@@ -107,10 +128,9 @@ const Map = () => {
             return;
           }
           if (stationOverlayRef.current) stationOverlayRef.current.setMap(null);
-          console.log("if 통과");
           const { Ma: lat, La: lng } = marker.getPosition();
           const moveLocation = new window.kakao.maps.LatLng(lat, lng);
-          map.panTo(moveLocation);
+          mapRef.current.panTo(moveLocation);
           setSelectedMarker(marker);
         });
 
@@ -123,16 +143,18 @@ const Map = () => {
         }
       }
     }
-  }, [stationList, map]);
+  }, [stationList]);
 
   // selectedMarker 변경시 충전소 세부 내용 data 요청
   useEffect(() => {
+    console.log("%c selectedMarker useEffect", "color:blue");
     if (selectedMarker) {
       const statId = selectedMarker.id;
 
       const fetchStationDetail = async (id) => {
         try {
           const data = await getStationDetail(id);
+          makeOverlay(data);
           setSelectedMarkerDetail(data);
         } catch (err) {
           throw err;
@@ -143,76 +165,72 @@ const Map = () => {
     }
   }, [selectedMarker]);
 
-  // 선택된 마커 상태 변경 시 데이터 렌더링
-  useEffect(() => {
-    // 오버레이 생성
-    if (selectedMarker && selectedMarkerDetail) {
-      const { addr, busiCall, useTime, busiNm, statId, lat, lng } =
-        selectedMarkerDetail;
+  function makeOverlay(detail) {
+    const { addr, busiCall, useTime, busiNm, statId, statNm, lat, lng } =
+      detail;
 
-      const overlayContainer = document.createElement("div");
-      overlayContainer.className = "overlay__container";
-      const titleWrapper = document.createElement("div");
-      titleWrapper.className = "title__wrapper";
-      const title = document.createElement("h1");
-      title.innerText = selectedMarkerDetail.statNm;
-      const closeButton = document.createElement("button");
-      closeButton.innerText = "닫기";
+    const overlayContainer = document.createElement("div");
+    overlayContainer.className = "overlay__container";
+    const titleWrapper = document.createElement("div");
+    titleWrapper.className = "title__wrapper";
+    const title = document.createElement("h1");
+    title.innerText = statNm;
+    const closeButton = document.createElement("button");
+    closeButton.innerText = "닫기";
 
-      const contentWrapper = document.createElement("div");
-      contentWrapper.className = "content__wrapper";
-      const contentListWrapper = document.createElement("ul");
-      contentListWrapper.className = "content-list__wrapper";
+    const contentWrapper = document.createElement("div");
+    contentWrapper.className = "content__wrapper";
+    const contentListWrapper = document.createElement("ul");
+    contentListWrapper.className = "content-list__wrapper";
 
-      const liAddress = document.createElement("li");
-      liAddress.innerText = `주소 : ${addr}`;
-      const liAddressDetail = document.createElement("li");
-      liAddressDetail.innerText = `상세주소 : ${addr}`;
-      const liTel = document.createElement("li");
-      liTel.innerText = `전화번호 : ${busiCall}`;
-      const liTime = document.createElement("li");
-      liTime.innerText = `운영시간 : ${useTime}`;
-      const liCompany = document.createElement("li");
-      liCompany.innerText = `운영기관 : ${busiNm}`;
-      const liCharger = document.createElement("li");
-      liCharger.innerText = `충전가능갯수 : `;
+    const liAddress = document.createElement("li");
+    liAddress.innerText = `주소 : ${addr}`;
+    const liAddressDetail = document.createElement("li");
+    liAddressDetail.innerText = `상세주소 : ${addr}`;
+    const liTel = document.createElement("li");
+    liTel.innerText = `전화번호 : ${busiCall}`;
+    const liTime = document.createElement("li");
+    liTime.innerText = `운영시간 : ${useTime}`;
+    const liCompany = document.createElement("li");
+    liCompany.innerText = `운영기관 : ${busiNm}`;
+    const liCharger = document.createElement("li");
+    liCharger.innerText = `충전가능갯수 : `;
 
-      contentListWrapper.appendChild(liAddress);
-      contentListWrapper.appendChild(liAddressDetail);
-      contentListWrapper.appendChild(liTel);
-      contentListWrapper.appendChild(liTime);
-      contentListWrapper.appendChild(liCompany);
-      contentListWrapper.appendChild(liCharger);
+    contentListWrapper.appendChild(liAddress);
+    contentListWrapper.appendChild(liAddressDetail);
+    contentListWrapper.appendChild(liTel);
+    contentListWrapper.appendChild(liTime);
+    contentListWrapper.appendChild(liCompany);
+    contentListWrapper.appendChild(liCharger);
 
-      contentWrapper.appendChild(contentListWrapper);
+    contentWrapper.appendChild(contentListWrapper);
 
-      titleWrapper.appendChild(title);
-      titleWrapper.appendChild(closeButton);
+    titleWrapper.appendChild(title);
+    titleWrapper.appendChild(closeButton);
 
-      overlayContainer.appendChild(titleWrapper);
-      overlayContainer.appendChild(contentWrapper);
+    overlayContainer.appendChild(titleWrapper);
+    overlayContainer.appendChild(contentWrapper);
 
-      const position = new window.kakao.maps.LatLng(lat, lng);
-      const newStationOverlay = new window.kakao.maps.CustomOverlay({
-        position,
-        content: overlayContainer,
-        yAnchor: 1.2,
-      });
+    const position = new window.kakao.maps.LatLng(lat, lng);
+    const newStationOverlay = new window.kakao.maps.CustomOverlay({
+      position,
+      content: overlayContainer,
+      yAnchor: 1.2,
+    });
 
-      newStationOverlay.id = statId;
+    newStationOverlay.id = statId;
 
-      newStationOverlay.setMap(map);
-      stationOverlayRef.current = newStationOverlay;
+    newStationOverlay.setMap(mapRef.current);
+    stationOverlayRef.current = newStationOverlay;
 
-      closeButton.addEventListener("click", () => {
-        // newStationOverlay.setMap(null);
-        stationOverlayRef.current.setMap(null);
-        stationOverlayRef.current = null;
-        setSelectedMarker(null);
-        setSelectedMarkerDetail(null);
-      });
-    }
-  }, [selectedMarkerDetail]);
+    closeButton.addEventListener("click", () => {
+      // newStationOverlay.setMap(null);
+      stationOverlayRef.current.setMap(null);
+      stationOverlayRef.current = null;
+      setSelectedMarker(null);
+      setSelectedMarkerDetail(null);
+    });
+  }
 
   return <div id="map" className="w-[calc(100%-390px)]"></div>;
 };
--- pages/api/station.js
+++ pages/api/station.js
@@ -1,6 +1,6 @@
 // sqlite 연결 하기 위한 라이브러리 import
-import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
 /**
  * @swagger
@@ -12,26 +12,28 @@ import { open } from 'sqlite'
  *         description: 충전소 정보의 배열
  */
 export default async function handler(req, res) {
+  // s: 좌측하단 위도, w: 좌측하단 경도, n: 우측상단 위도, e: 우측상단 경도
   const { zcode, zscode, kind, kindDetail, dataType, s, w, n, e } = req.query;
-  console.log("req:",req.query);
+  console.log("req:", req.query);
 
-  const db = await  open({
-    filename:  './db.sqlite' ,
-    driver: sqlite3.Database
-  })
+  const db = await open({
+    filename: "./db.sqlite",
+    driver: sqlite3.Database,
+  });
 
-  let condition = zcode?` and zcode="${zcode}"`:"";
-  condition += zscode?` and zscode="${zscode}"`:"";
-  condition += kind?` and kind="${kind}"`:"";
-  condition += kindDetail?` and kindDetail="${kindDetail}"`:"";
-  condition += dataType?` and dataType="${dataType}"`:"";
-  condition += s?` and lat>"${s}"`:"";
-  condition += w?` and lng>"${w}"`:"";
-  condition += n?` and lat<"${n}"`:"";
-  condition += e?` and lng<"${e}"`:"";
+  let condition = zcode ? ` and zcode="${zcode}"` : "";
+  condition += zscode ? ` and zscode="${zscode}"` : "";
+  condition += kind ? ` and kind="${kind}"` : "";
+  condition += kindDetail ? ` and kindDetail="${kindDetail}"` : "";
+  condition += dataType ? ` and dataType="${dataType}"` : "";
+  condition += s ? ` and lat>"${s}"` : "";
+  condition += w ? ` and lng>"${w}"` : "";
+  condition += n ? ` and lat<"${n}"` : "";
+  condition += e ? ` and lng<"${e}"` : "";
   console.log(`select * from Station where ""="" ${condition}`);
 
   const Chargers = await db.all(`select * from Station where 1=1 ${condition}`);
   // const Chargers = await db.all('select * from Station')
+  console.log(Chargers);
   res.status(200).json(Chargers);
 }
