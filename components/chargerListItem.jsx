2fef0abd21b13d1b8936addc74c2c0d7e4ceb757
dldnjswns31 <dldnjswns32@gmail.com>
2023-07-18 00:49:05 +0900

    충전기 리스트 작업

--- components/chargerListItem.jsx
+++ components/chargerListItem.jsx
@@ -1,7 +1,104 @@
 import React from "react";
+import Image from "next/image";
 
-const ChargerListItem = () => {
-  return <li className="w-full h-20 mb-3  rounded-xl bg-white"></li>;
+import { CHARGER_STAT, CHARGER_TYPE } from "../constants";
+import AC3 from "./svg/ac3";
+import ACFullCharge from "./svg/acFullCharge";
+import DCChademo from "./svg/dcChademo";
+import DCCombo from "./svg/dcCombo";
+
+const STAT_COLOR = {
+  1: "text-red-500", // 통신이상
+  2: "text-green-500", // 충전대기
+  3: "text-yellow-500", // 충전중
+  4: "text-black", // 운영중지
+  5: "text-gray-500", // 점검중
+  9: "text-gray-500", // 상태미확인
+};
+
+const ChargerListItem = ({ charger }) => {
+  const { stat, chgerType } = charger;
+  return (
+    <li className="flex w-full h-20 mb-3  rounded-xl bg-white">
+      <div
+        className={`flex grow justify-center items-center font-bold ${STAT_COLOR[stat]}`}
+      >
+        <span>{CHARGER_STAT[stat]}</span>
+      </div>
+      <div className="flex flex-col max-w-[64px] grow justify-center items-center">
+        <AC3
+          fill={
+            CHARGER_TYPE[chgerType].includes("AC3상") ? "black" : "gainsboro"
+          }
+          width={32}
+          height={32}
+        />
+        <span
+          className={`text-[8px] font-bold ${
+            CHARGER_TYPE[chgerType].includes("AC3상")
+              ? "text-black"
+              : "text-gray-300"
+          }`}
+        >
+          AC3상
+        </span>
+      </div>
+      <div className="flex flex-col max-w-[64px] grow justify-center items-center">
+        <ACFullCharge
+          fill={
+            CHARGER_TYPE[chgerType].includes("AC완속") ? "black" : "gainsboro"
+          }
+          width={32}
+          height={32}
+        />
+        <span
+          className={`text-[8px] font-bold ${
+            CHARGER_TYPE[chgerType].includes("AC완속")
+              ? "text-black"
+              : "text-gray-300"
+          }`}
+        >
+          AC완속
+        </span>
+      </div>
+      <div className="flex flex-col max-w-[64px] grow justify-center items-center">
+        <DCChademo
+          fill={
+            CHARGER_TYPE[chgerType].includes("DC차데모") ? "black" : "gainsboro"
+          }
+          width={32}
+          height={32}
+        />
+        <span
+          className={`text-[8px] font-bold ${
+            CHARGER_TYPE[chgerType].includes("DC차데모")
+              ? "text-black"
+              : "text-gray-300"
+          }`}
+        >
+          DC차데모
+        </span>
+      </div>
+      <div className="flex flex-col max-w-[64px] grow justify-center items-center">
+        <DCCombo
+          fill={
+            CHARGER_TYPE[chgerType].includes("DC콤보") ? "black" : "gainsboro"
+          }
+          width={32}
+          height={32}
+        />
+        <span
+          className={`text-[8px] font-bold ${
+            CHARGER_TYPE[chgerType].includes("DC콤보")
+              ? "text-black"
+              : "text-gray-300"
+          }`}
+        >
+          DC콤보
+        </span>
+      </div>
+    </li>
+  );
 };
 
 export default ChargerListItem;
---
+++ components/svg/DCCombo.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const DCCombo = ({ fill, width, height }) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill={fill}
+      width={width}
+      height={height}
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M12,14A6,6,0,1,0,6,8,6,6,0,0,0,12,14ZM12,3.5A4.5,4.5,0,1,1,7.5,8,4.49,4.49,0,0,1,12,3.5ZM12.75,6a1,1,0,1,1,1,1A1,1,0,0,1,12.75,6Zm2,1.75A.75.75,0,1,1,14,8.5.76.76,0,0,1,14.75,7.75ZM11,10.5a1,1,0,1,1,1,1A1,1,0,0,1,11,10.5ZM9.25,9.25A.75.75,0,1,1,10,8.5.76.76,0,0,1,9.25,9.25ZM9.25,6a1,1,0,1,1,1,1A1,1,0,0,1,9.25,6Zm6.25,9h-7a3.5,3.5,0,0,0,0,7h7a3.5,3.5,0,0,0,0-7Zm0,5.5h-7a2,2,0,0,1,0-4h7a2,2,0,0,1,0,4ZM9,17a1.5,1.5,0,1,0,1.5,1.5A1.5,1.5,0,0,0,9,17Zm0,2a.5.5,0,1,1,.5-.5A.5.5,0,0,1,9,19Zm6.25-2a1.5,1.5,0,1,0,1.5,1.5A1.5,1.5,0,0,0,15.25,17Zm0,2a.5.5,0,1,1,.5-.5A.5.5,0,0,1,15.25,19Z"
+          fill-rule="evenodd"
+        ></path>
+        <rect width="24" height="24" fill="none"></rect>
+      </g>
+    </svg>
+  );
+};
+
+export default DCCombo;
