453184800378bacbeee58a4ca46b3366142dfd75
dldnjswns31 <dldnjswns32@gmail.com>
2023-07-24 14:44:02 +0900

    충전소 충전기 리스트 스크롤 안되는 현상 수정

--- components/sidebar/chargerList.jsx
+++ components/sidebar/chargerList.jsx
@@ -7,7 +7,7 @@ import { selectedMarkerDetailAtom } from "../../atoms/atom";
 const ChargerList = () => {
   const selectedMarkerDetail = useAtomValue(selectedMarkerDetailAtom);
   return (
-    <div>
+    <div className="w-full h-full">
       <div className="w-full h-60 mb-3 bg-white rounded-2xl"></div>
       <div className="h-[calc(100%-240px)] overflow-y-scroll no-scrollbar">
         <ul>
