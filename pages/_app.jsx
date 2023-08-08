e5ba8b8fef18f8959678c67a126667f8cce036a4
dldnjswns31 <dldnjswns32@gmail.com>
2023-08-08 09:14:02 +0900

    title, favicon을 document에서 app으로 이동

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,15 @@
 import Layout from "../components/common/layout";
 import "../styles/globals.css";
 import "../styles/components/map.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>전기차 충전소 조회</title>
+        <link rel="icon" href="/ocube.png" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,10 +4,7 @@ import React from "react";
 export default function _document() {
   return (
     <Html lang="ko">
-      <Head>
-        <title>전기차 충전소 조회</title>
-        <link rel="icon" href="/ocube.png" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
