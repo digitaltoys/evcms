6fe8b49c662af7fe6a7e53ec77c0f034f75a90cd
dldnjswns31 <dldnjswns32@gmail.com>
2023-08-11 17:20:47 +0900

    Navbar 컴포넌트 navigation button 하이라이트 추가 - 현재 위치가 해당 버튼이 routing하는 위치라면, 버튼 색 변경

--- components/common/navbar.jsx
+++ components/common/navbar.jsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 const Navbar = () => {
+  const router = useRouter();
+
+  function isActive(path) {
+    return router.pathname === path ? true : false;
+  }
+
   return (
     <nav className="flex justify-between items-center  w-full h-16 bg-white shadow-md">
       <div className="flex items-center gap-4 p-4">
@@ -15,6 +22,16 @@ const Navbar = () => {
         <span className="text-2xl font-bold">EVCMS</span>
       </div>
       <div className="px-4">
+        <Link className={`px-4`} href="/">
+          <span className={`${isActive("/") && "text-green-500"}`}>
+            충전소 조회
+          </span>
+        </Link>
+        <Link className={`px-4`} href="/user/uselist">
+          <span className={`${isActive("/user/uselist") && "text-green-500"}`}>
+            이용내역
+          </span>
+        </Link>
         <Link
           href="/login"
           className="py-2 px-4 text-sm font-bold border-[1px]"
